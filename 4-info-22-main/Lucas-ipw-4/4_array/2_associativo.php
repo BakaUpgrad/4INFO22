eb1eda870dfaa550c10f03e5f5892ff9c8e06162
BakaUpgrad <108496889+BakaUpgrad@users.noreply.github.com>
2022-07-07 21:10:32 -0300

    Add files via upload

---
+++ 4-info-22-main/Lucas-ipw-4/4_array/2_associativo.php
@@ -0,0 +1,16 @@
+<?php
+
+$pessoa = array();
+
+$pessoa ['nome']='Matheus Pereira';
+$pessoa ['email']='MatheusP@escola.pr.gov.br';
+$pessoa ['telefone']='99834562'; 
+
+
+
+echo $pessoa['nome'];
+echo '<br>';
+echo $pessoa ['email'];
+echo '<br>';
+echo $pessoa ['telefone'];
+?>
